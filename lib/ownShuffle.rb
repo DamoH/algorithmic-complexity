d20a5cc4c8c3b622b1275f4ad965f779a7642c41
Damion Hardy <damionhardy@Damos-MBP-8.home>
2018-11-06 20:19:20 +0000

    ownShuffle algo being timed correctly with nanoseconds calc adjusted - chart plotted

--- lib/ownShuffle.rb
+++ lib/ownShuffle.rb
@@ -10,15 +10,16 @@ class OwnShuffle
     # working out the time difference '100' times...
     for i in 1..100 do
       # myArray.shuffle    # remove when not timing the sort method
-      a = Time.now.nsec
+      a = Time.now.to_f
       # own shuffle algo here....
-        len = (myArray.length) * 0.1
-        for i in 1..len do
-          myArray.insert(rand(len), myArray.delete_at(rand(len)))
+        len = (myArray.length) * 0.005
+        for i in 1..len do # always shuffled 0.5% times of array size
+          myArray.insert(0, myArray.delete_at(rand(len)))
         end
       # **************************
-      b = Time.now.nsec
+      b = Time.now.to_f
       results << (b - a)
+      # p "b: #{b} - a: #{a} = #{b - a}"
       i += 1
     end
     @finalArray = results.sort.pop(90).shift(80)
@@ -30,7 +31,7 @@ class OwnShuffle
   def runner
     (50000..1000000).step(50000) do |n|
       shuffleCalc(n)
-      puts "Average for #{n} = #{@finalArray.reduce(:+) / @finalArray.size.to_f}"
+      puts "Average for #{n} = #{((@finalArray.reduce(:+) / @finalArray.size.to_f) * 1000000000)}"
     end
   end
 
